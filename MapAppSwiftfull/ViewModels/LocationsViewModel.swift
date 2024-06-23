ad45bb81a6e6311dfc6cc9892c58a8e26275ff53
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-06-23 19:54:32 +0200

    [#3] Set default map location

--- MapAppSwiftfull/ViewModels/LocationsViewModel.swift
+++ MapAppSwiftfull/ViewModels/LocationsViewModel.swift
@@ -6,13 +6,27 @@
 //
 
 import Foundation
+import MapKit
+import _MapKit_SwiftUI
 
 class LocationsViewModel: ObservableObject {
-    
+    // All loaded locations
     @Published var locations: [Location]
+    // Current location on map
+    @Published var mapLocation: Location
+    @Published var mapRegion: MapCameraPosition = MapCameraPosition.automatic
+    let mapSpam = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    private func updateMapRegion(location:Location) {
+    
+            mapRegion = MapCameraPosition.region(MKCoordinateRegion(
+                center:location.coordinates,
+                span: mapSpam))
     }
 }
