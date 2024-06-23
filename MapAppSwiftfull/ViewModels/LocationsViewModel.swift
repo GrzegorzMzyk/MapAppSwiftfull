cab253b97bee4d95daa81d3db88f08f133138523
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-06-23 20:09:09 +0200

    [#3] added "didSet"

--- MapAppSwiftfull/ViewModels/LocationsViewModel.swift
+++ MapAppSwiftfull/ViewModels/LocationsViewModel.swift
@@ -9,10 +9,16 @@ import Foundation
 import _MapKit_SwiftUI
 
 class LocationsViewModel: ObservableObject {
+    
     // All loaded locations
     @Published var locations: [Location]
     // Current location on map
-    @Published var mapLocation: Location
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
     @Published var mapRegion: MapCameraPosition = MapCameraPosition.automatic
     
     let mapSpam = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
